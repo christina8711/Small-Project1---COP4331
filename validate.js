c7758eda604eb7910d329a1f698a8ce9a59600f5
Bruno Fregoso <bruno88756@gmail.com>
2024-08-30 15:43:09 -0400

    added form validation

---
+++ validate.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("login-form-id");
+const fname = document.getElementById("first-name");
+const lname = document.getElementById("last-name");
+const username = document.getElementById("user-name");
+const password = document.getElementById("pass-word");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
